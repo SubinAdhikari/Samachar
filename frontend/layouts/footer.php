c39e5b29b481d9953900d6e0152aacbb10aaf5b4
SubinAdhikari <subinadhikari61@gmail.com>
2020-04-09 12:23:47 +0545

    avisek modified

--- frontend/layouts/footer.php
+++ frontend/layouts/footer.php
@@ -1,3 +1,4 @@
+
 <footer>
 		<div class="bg2 p-t-40 p-b-25">
 			<div class="container">
@@ -12,32 +13,32 @@
 						<div>
 							<br>
 							<p class="f1-s-1 cl11 p-b-16">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempor magna eget elit efficitur, at accumsan sem placerat. Nulla tellus libero, mattis nec molestie at, facilisis ut turpis. Vestibulum dolor metus, tincidunt eget odio
+								Registration No: 1424/56789<br>
+								MountTech Solutions<br>
+								Subin Adhikari<br>
+									   Shreedhar Bhandari<br>
+									   Abhishek Karki
 							</p>
 
 							<p class="f1-s-1 cl11 p-b-16">
-								Any questions? Call us on (+1) 96 716 6879
+								Any questions? Call us on 9808832123
 							</p>
 
 							<div class="p-t-15">
 								<a href="#" class="fs-18 cl11 hov-cl10 trans-03 m-r-8">
-									<span class="fab fa-facebook-f"></span>
+									<i class="fab fa-facebook"></i>
 								</a>
 
 								<a href="#" class="fs-18 cl11 hov-cl10 trans-03 m-r-8">
-									<span class="fab fa-twitter"></span>
+									<i class="fab fa-twitter-square"></i>
 								</a>
 
 								<a href="#" class="fs-18 cl11 hov-cl10 trans-03 m-r-8">
-									<span class="fab fa-pinterest-p"></span>
+									<i class="fab fa-google-plus-square"></i>
 								</a>
 
 								<a href="#" class="fs-18 cl11 hov-cl10 trans-03 m-r-8">
-									<span class="fab fa-vimeo-v"></span>
-								</a>
-
-								<a href="#" class="fs-18 cl11 hov-cl10 trans-03 m-r-8">
-									<span class="fab fa-youtube"></span>
+									<i class="fab fa-youtube-square"></i>
 								</a>
 							</div>
 						</div>
@@ -46,105 +47,66 @@
 					<div class="col-sm-6 col-lg-4 p-b-20">
 						<div class="size-h-3 flex-s-c">
 							<h5 class="f1-m-7 cl0">
-								Popular Posts
+								Exchange Rates
 							</h5>
 						</div>
 
-						<ul>
-							<li class="flex-wr-sb-s p-b-20">
-								<a href="#" class="size-w-4 wrap-pic-w hov1 trans-03">
-									<img src="images/popular-post-01.jpg" alt="IMG">
-								</a>
+						
+	<link rel="stylesheet" type="text/css" href="//www.exchangerates.org.uk/widget/ER-SCRT2-css.php?w=200&nb=10&bdrc=E0E0E0&mbg=FFFFFF&fc=333333&tc=333333" media="screen" />
+				<div id="erscrt2">
+					<div id="erscrt2-widget">
+					</div>
+					<div id="erscrt2-infolink">
+						<a rel="nofollow" href="//www.exchangerates.org.uk/Nepalese-Rupee-NPR-currency-table.html" target="_new" ><img src='https://www.exchangerates.org.uk/widget/logo-333333.png' alt='ExchangeRates.org.uk'></a>
+					</div>
+						<script type="text/javascript">	
+								var tz = 'userset';
+								var w = '220';
+								var mc = 'NPR';
+								var nb = '10';
+								var c1 = 'USD';
+								var c2 = 'EUR';
+								var c3 = 'AUD';
+								var c4 = 'JPY';
+								var c5 = 'INR';
+								var c6 = 'CAD';
+								var c7 = 'ZAR';
+								var c8 = 'NZD';
+								var c9 = 'SGD';
+								var c10 = 'CNY';
+								var t = 'Live Exchange Rates';
+								var tc = '333333';
+								var bdrc = 'E0E0E0';
+								var mbg = 'FFFFFF';
+								var fc = '333333';
 
-								<div class="size-w-5">
-									<h6 class="p-b-5">
-										<a href="#" class="f1-s-5 cl11 hov-cl10 trans-03">
-											Donec metus orci, malesuada et lectus vitae
-										</a>
-									</h6>
-
-									<span class="f1-s-3 cl6">
-										Feb 17
-									</span>
-								</div>
-							</li>
-
-							<li class="flex-wr-sb-s p-b-20">
-								<a href="#" class="size-w-4 wrap-pic-w hov1 trans-03">
-									<img src="images/popular-post-02.jpg" alt="IMG">
-								</a>
-
-								<div class="size-w-5">
-									<h6 class="p-b-5">
-										<a href="#" class="f1-s-5 cl11 hov-cl10 trans-03">
-											Lorem ipsum dolor sit amet, consectetur
-										</a>
-									</h6>
-
-									<span class="f1-s-3 cl6">
-										Feb 16
-									</span>
-								</div>
-							</li>
-
-							<li class="flex-wr-sb-s p-b-20">
-								<a href="#" class="size-w-4 wrap-pic-w hov1 trans-03">
-									<img src="images/popular-post-03.jpg" alt="IMG">
-								</a>
-
-								<div class="size-w-5">
-									<h6 class="p-b-5">
-										<a href="#" class="f1-s-5 cl11 hov-cl10 trans-03">
-											Suspendisse dictum enim quis imperdiet auctor
-										</a>
-									</h6>
-
-									<span class="f1-s-3 cl6">
-										Feb 15
-									</span>
-								</div>
-							</li>
-						</ul>
+								var ccHost = (("https:" == document.location.protocol) ? "https://www." : "http://www.");
+								document.write(unescape("%3Cscript src='" + ccHost + "exchangerates.org.uk/widget/ER-SCRT2-1.php' type='text/javascript'%3E%3C/script%3E"));
+						</script>
+           		</div>
+<!-- Simple Currency Rates Table END -->
+<!-- Simple Currency Rates Table END -->
 					</div>
 
 					<div class="col-sm-6 col-lg-4 p-b-20">
+						<?php $result=getAllCategories($conn);
+						?>
 						<div class="size-h-3 flex-s-c">
 							<h5 class="f1-m-7 cl0">
 								Category
 							</h5>
 						</div>
-
+						<?php
+                          foreach($result as $key){ 
+                          	?>
 						<ul class="m-t--12">
 							<li class="how-bor1 p-rl-5 p-tb-10">
-								<a href="#" class="f1-s-5 cl11 hov-cl10 trans-03 p-tb-8">
-									Fashion (22)
-								</a>
-							</li>
-
-							<li class="how-bor1 p-rl-5 p-tb-10">
-								<a href="#" class="f1-s-5 cl11 hov-cl10 trans-03 p-tb-8">
-									Technology (29)
-								</a>
-							</li>
-
-							<li class="how-bor1 p-rl-5 p-tb-10">
-								<a href="#" class="f1-s-5 cl11 hov-cl10 trans-03 p-tb-8">
-									Street Style (15)
-								</a>
-							</li>
-
-							<li class="how-bor1 p-rl-5 p-tb-10">
-								<a href="#" class="f1-s-5 cl11 hov-cl10 trans-03 p-tb-8">
-									Life Style (28)
-								</a>
-							</li>
-
-							<li class="how-bor1 p-rl-5 p-tb-10">
-								<a href="#" class="f1-s-5 cl11 hov-cl10 trans-03 p-tb-8">
-									DIY & Crafts (16)
+								<a href="category-02.php?ref=<?php echo $key['category_id'];  ?>" class="f1-s-5 cl11 hov-cl10 trans-03 p-tb-8">
+									<?php echo $key['category_name']; ?>
 								</a>
 							</li>
 						</ul>
+					<?php } ?>
 					</div>
 				</div>
 			</div>

64770b9988c4f9688bff47796aac6f5293639505
SubinAdhikari <subinadhikari61@gmail.com>
2020-04-27 09:45:23 +0545

    Nepali exchange rate

--- EnglishFrontend/layouts/footer.php
+++ EnglishFrontend/layouts/footer.php
@@ -54,35 +54,10 @@
 						
 	<link rel="stylesheet" type="text/css" href="//www.exchangerates.org.uk/widget/ER-SCRT2-css.php?w=200&nb=10&bdrc=E0E0E0&mbg=FFFFFF&fc=333333&tc=333333" media="screen" />
 				<div id="erscrt2">
-					<div id="erscrt2-widget">
-					</div>
-					<div id="erscrt2-infolink">
-						<a rel="nofollow" href="//www.exchangerates.org.uk/Nepalese-Rupee-NPR-currency-table.html" target="_new" ><img src='https://www.exchangerates.org.uk/widget/logo-333333.png' alt='ExchangeRates.org.uk'></a>
-					</div>
-						<script type="text/javascript">	
-								var tz = 'userset';
-								var w = '220';
-								var mc = 'NPR';
-								var nb = '10';
-								var c1 = 'USD';
-								var c2 = 'EUR';
-								var c3 = 'AUD';
-								var c4 = 'JPY';
-								var c5 = 'INR';
-								var c6 = 'CAD';
-								var c7 = 'ZAR';
-								var c8 = 'NZD';
-								var c9 = 'SGD';
-								var c10 = 'CNY';
-								var t = 'Live Exchange Rates';
-								var tc = '333333';
-								var bdrc = 'E0E0E0';
-								var mbg = 'FFFFFF';
-								var fc = '333333';
-
-								var ccHost = (("https:" == document.location.protocol) ? "https://www." : "http://www.");
-								document.write(unescape("%3Cscript src='" + ccHost + "exchangerates.org.uk/widget/ER-SCRT2-1.php' type='text/javascript'%3E%3C/script%3E"));
-						</script>
+					
+				<iframe src="https://www.ashesh.com.np/forex/widget2.php?api=792047j282" frameborder="0" scrolling="no" marginwidth="0" marginheight="0" style="border:none; overflow:hidden; width:100%; height:383px; border-radius:5px;" allowtransparency="true">
+</iframe><br><span style="text-align:left">© <a href="https://www.ashesh.com.np/forex/" title="Forex Nepal for Nepalese Rupee" target="_top" style="text-decoration:none;">Forex Nepal</a></span>
+						
            		</div>
 <!-- Simple Currency Rates Table END -->
 <!-- Simple Currency Rates Table END -->
